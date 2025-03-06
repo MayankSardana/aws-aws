3d6fe0eab559db7119a7d4c7ce39d1644fd8ea03
mayank_sardana <mayank_sardana@epam.com>
2025-03-06 19:55:03 +0530

    econgg

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,8 +1,26 @@
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+    const path = event?.rawPath || "Unknown";
+    const method = event?.requestContext?.http?.method || "Unknown";
+
+    // Allow only GET /hello
+    if (method === "GET" && path === "/hello") {
+        return {
+            statusCode: 200,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                statusCode: 200,
+                message: "Hello from Lambda"
+            })
+        };
+    }
+
+    // Return 400 Bad Request for all other cases
+    return {
+        statusCode: 400,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            statusCode: 400,
+            message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
+        })
     };
-    return response;
 };
